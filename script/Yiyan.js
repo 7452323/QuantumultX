7c1d01e1007a78919af08bc4bce612d6ff58884e
Reviewa <141471346+Reviewa@users.noreply.github.com>
2026-05-18 17:21:46 +0800

    更新 Yiyan.js

--- script/Yiyan.js
+++ script/Yiyan.js
@@ -1,10 +1,10 @@
-
+```
 /*
 一言 - 记录字句，发现共鸣
 https://apps.apple.com/cn/app/id1010174792
 
 [rewrite_local]
-^https:\/\/app\.yiyan\.art\/yiyan\/getuserinfoandbooklist url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/yiyan.js
+^https:\/\/app\.yiyan\.art\/yiyan\/(getuserinfoandbooklist|checkpro|getvipproduct) url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/yiyan.js
 
 [mitm]
 hostname = app.yiyan.art
@@ -12,12 +12,19 @@ hostname = app.yiyan.art
 
 var obj = JSON.parse($response.body);
 
-if (obj.u) {
-  obj.u.viptype = "4";
-  obj.u.device = "0";
+if (obj.viptype) {
+  obj.viptype = "4";
+  obj.device = "0";
+}
+
+if (obj.user) {
+  obj.user.viptype = "4";
+  obj.user.device = "0";
 }
 
 if (obj.product) {
+  obj.u.viptype = "4";
+  obj.u.device = "0";
   obj.product.permanenttitle = "终身会员";
   obj.product.permanentdesc = "已解锁永久会员";
   obj.product.permanentprice = "0";
