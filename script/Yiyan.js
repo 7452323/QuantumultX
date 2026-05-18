5293eb966935379b911937f1cf7072b1f8ff0081
Reviewa <141471346+Reviewa@users.noreply.github.com>
2026-05-18 16:50:13 +0800

    更新 Yiyan.js

--- script/Yiyan.js
+++ script/Yiyan.js
@@ -7,27 +7,31 @@
 hostname = app.yiyan.art
 
 */
-let obj = JSON.parse($response.body);
+var obj = JSON.parse($response.body);
 
-if (obj.u) {
-  obj.u.viptype = "4"; // 解锁 VIP
-  obj.u.device = "0";
+if (obj.data) {
+  obj.data.is_vip = 1;
+  obj.data.isVip = true;
+  obj.data.vipLevel = 1;
+  obj.data.vipType = "终身会员";
+  obj.data.isPremium = true;
+  obj.data.auto_renew = 1;
+
+  obj.data.vipEndTime = "2099-12-31 23:59:59";
+  obj.data.vip_end = 4092599349;
+  obj.data.vipDay = "9999";
+  obj.data.vipExpireDate = "2099-12-31";
+
+  obj.data.coin = "99999";
+  obj.data.coins = 99999;
+  obj.data.freeCount = "9999";
 }
 
-if (obj.product) {
-  obj.product.permanenttitle = "终身会员";
-  obj.product.permanentdesc = "已解锁永久会员";
-  obj.product.permanentprice = "0";
-
-  obj.product.yearprice = "0";
-  obj.product.yeartitle = "";
-  obj.product.yeardesc = "";
-
-  obj.product.yearprice_origin = "0";
-  obj.product.permanentprice_origin = "0";
+if (obj.is_vip != null) {
+  obj.is_vip = 1;
 }
-if (obj.banner) {
-  obj.banner = [];
+if (obj.isVip != null) {
+  obj.isVip = true;
 }
 
 $done({ body: JSON.stringify(obj) });
