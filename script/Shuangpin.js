b5f13acfa80aad0da3e517526220e3e941ab14a9
7452323 <141471346+7452323@users.noreply.github.com>
2026-05-31 00:43:32 +0800

    catch ALL RC API v1/*

--- script/Shuangpin.js
+++ script/Shuangpin.js
@@ -1,10 +1,10 @@
 /*
  * @name Shuangpin PRO
- * @description RevenueCat 4.x 永久解锁
+ * @description RevenueCat 4.x 永久解锁 - 拦截全API
 
  [rewrite_local]
-^https?:\/\/api\.(revenuecat|rc-backup)\.com\/v1\/(subscribers\/[^?#]+|receipts) url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Shuangpin.js
-^https?:\/\/api\.(revenuecat|rc-backup)\.com\/v1\/ url script-response-header https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Shuangpin.js
+^https?:\/\/api\.(revenuecat|rc-backup)\.com\/v1\/.* url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Shuangpin.js
+^https?:\/\/api\.(revenuecat|rc-backup)\.com\/v1\/.* url script-response-header https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Shuangpin.js
 
  [mitm]
  hostname = api.revenuecat.com, api.rc-backup.com
@@ -14,13 +14,17 @@
 // Header mode
 if (!$response.body) {
   var h = $response.headers;
-  if (h && h['x-signature']) { delete h['x-signature']; }
+  if (h) {
+    delete h['x-signature'];
+    delete h['etag'];
+    delete h['x-revenuecat-etag'];
+    h['Cache-Control'] = 'no-cache';
+  }
   $done({headers: h});
   return;
 }
 
-// Body mode
-var url = $request.url;
+// Body mode - 所有RevenueCat API都注入PRO
 var body = $response.body;
 if (!body) { $done({}); }
 
@@ -30,23 +34,17 @@ try {
   var pro = { expires_date: null, product_identifier: "ulpb_lifetime_personal", purchase_date: now };
   var sub = { expires_date: null, period_type: "normal", purchase_date: now, store: "app_store" };
 
-  if (url.indexOf('/subscribers/') > -1 && url.indexOf('/attributes') < 0 && url.indexOf('/offerings') < 0) {
-    if (obj.subscriber) {
-      obj.subscriber.entitlements = { pro: pro };
-      obj.subscriber.subscriptions = { ulpb_lifetime_personal: sub };
-    }
-    $done({body: JSON.stringify(obj)});
+  // subscriber端点
+  if (obj.subscriber) {
+    obj.subscriber.entitlements = { pro: pro };
+    obj.subscriber.subscriptions = { ulpb_lifetime_personal: sub };
+    obj.subscriber.non_subscriptions = {};
+    obj.subscriber.entitlements_by_product_ids = {};
   }
-  else if (url.indexOf('/offerings') > -1) {
+  // offerings端点
+  if (obj.offerings) {
     obj.subscriber = { entitlements: { pro: pro }, subscriptions: { ulpb_lifetime_personal: sub } };
-    $done({body: JSON.stringify(obj)});
   }
-  else if (url.indexOf('/receipts') > -1) {
-    if (obj.subscriber) {
-      obj.subscriber.entitlements = { pro: pro };
-      obj.subscriber.subscriptions = { ulpb_lifetime_personal: sub };
-    }
-    $done({body: JSON.stringify(obj)});
-  }
-  else { $done({}); }
+
+  $done({body: JSON.stringify(obj)});
 } catch(e) { $done({}); }
