eabba8b8052f9239fbe66e488748530818d5f8cc
7452323 <141471346+7452323@users.noreply.github.com>
2026-05-31 02:49:52 +0800

    更新 Lanfan.js

--- script/Lanfan.js
+++ script/Lanfan.js
@@ -18,6 +18,6 @@ if (!body) { $done({}); }
 body = body.replace(/"is_purchased":\w+/g, '"is_purchased":true');
 body = body.replace(/"is_prime":\w+/g, '"is_prime":true');
 body = body.replace(/"unlocked":\w+/g, '"unlocked":true');
-body = body.replace(/"watch_type":\d+/g, '"watch_type":1');
+body = body.replace(/"watch_type":\d+/g, '"watch_type":3');
 
 $done({body: body});
