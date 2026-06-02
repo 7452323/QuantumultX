1c7dce88975021476f0862a2dac7f9432fca212d
7452323 <7452323@users.noreply.github.com>
2026-06-02 15:26:13 -0400

    Lanfan: 加回视频高清替换 v.0_10000. -> v.

--- script/Lanfan.js
+++ script/Lanfan.js
@@ -1,17 +1,10 @@
 /*
- * @name 懒饭 PRO
- * @description 懒饭会员解锁
- * @compatible QuantumultX Surge Loon
+懒饭 会员解锁
+[rewrite_local]
+^https?:\/\/lanfanapp\.com\/api\/v1 url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Lanfan.js
 
- [rewrite_local]
-^https?:\/\/lanfanapp\.com\/api\/v1\/.* url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Lanfan.js
-
- [URL-rewrite]
-v\.0_10000\. v.
-
- [mitm]
- hostname = lanfanapp.com, video5.chuimg.com
-
+[mitm]
+hostname = lanfanapp.com
 */
 
 var body = $response.body;
@@ -20,7 +13,8 @@ if (!body) { $done({}); }
 body = body.replace(/"is_prime":\w+/g, '"is_prime":true');
 body = body.replace(/"unlocked":\w+/g, '"unlocked":true');
 body = body.replace(/"expires_time":"[^"]+"/g, '"expires_time":"2099-12-31 23:59:59"');
-body = body.replace(/v\.0_10000\./g, 'v.');
 body = body.replace(/"watermark":\w+/g, '"watermark":false');
+body = body.replace(/"is_purchased":\w+/g, '"is_purchased":true');
+body = body.replace(/v\.0_10000\./g, 'v.');
 
 $done({body: body});
