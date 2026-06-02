21eaa73d6d4494b005d1edd88c216c6e38209803
7452323 <7452323@users.noreply.github.com>
2026-06-02 14:45:02 -0400

    Lanfan: 恢复视频高清替换 (v.0_10000. -> v.)

--- script/Lanfan.js
+++ script/Lanfan.js
@@ -1,11 +1,14 @@
 /*
-懒饭 PRO — 会员解锁
+懒饭 PRO — 会员解锁 + 完整高清视频
 
 [rewrite_local]
 ^https?:\/\/lanfanapp\.com\/api\/v1 url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Lanfan.js
 
+[URL-rewrite]
+^https?:\/\/video5?\.chuimg\.com\/.+\/v\.0_10000\. /307 v.
+
 [mitm]
-hostname = lanfanapp.com
+hostname = lanfanapp.com, *.chuimg.com
 */
 
 let body = $response.body;
