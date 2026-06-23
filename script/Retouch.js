7ce2a3e1554b327b3328bff5489bf34010218136
7452323 <7452323@users.noreply.github.com>
2026-06-23 09:11:33 -0400

    narrow match to subscriber only, exclude offerings

--- script/Retouch.js
+++ script/Retouch.js
@@ -4,8 +4,8 @@
  * @author 7452323
  *
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/.+ url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Retouch.js
-^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/.+ url script-request-header https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Retouch.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+\/?$ url script-response-body https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Retouch.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+\/?$ url script-request-header https://raw.githubusercontent.com/7452323/QuantumultX/main/script/Retouch.js
 
 [mitm]
 hostname = api.revenuecat.com
@@ -18,28 +18,24 @@ if (typeof $response == "undefined") {
   $done({ headers: $request.headers });
 } else {
   var body = JSON.parse($response.body);
-  if (body && body.subscriber) {
-    body.subscriber.subscriptions = {
-      tr5_yearlysubsc_20dlrs_1: {
-        expires_date: "2099-12-31T23:59:59Z",
-        original_purchase_date: "2023-01-01T00:00:00Z",
-        purchase_date: "2024-01-01T00:00:00Z",
-        ownership_type: "PURCHASED",
-        store: "app_store",
-        is_sandbox: false,
-        unsubscribe_detected_at: null,
-        billing_issues_detected_at: null
-      }
-    };
-    body.subscriber.entitlements = {
-      premium: {
-        expires_date: "2099-12-31T23:59:59Z",
-        product_identifier: "tr5_yearlysubsc_20dlrs_1",
-        purchase_date: "2024-01-01T00:00:00Z"
-      }
-    };
-    $done({ body: JSON.stringify(body) });
-  } else {
-    $done({});
-  }
+  body.subscriber.subscriptions = {
+    tr5_yearlysubsc_20dlrs_1: {
+      expires_date: "2099-12-31T23:59:59Z",
+      original_purchase_date: "2023-01-01T00:00:00Z",
+      purchase_date: "2024-01-01T00:00:00Z",
+      ownership_type: "PURCHASED",
+      store: "app_store",
+      is_sandbox: false,
+      unsubscribe_detected_at: null,
+      billing_issues_detected_at: null
+    }
+  };
+  body.subscriber.entitlements = {
+    premium: {
+      expires_date: "2099-12-31T23:59:59Z",
+      product_identifier: "tr5_yearlysubsc_20dlrs_1",
+      purchase_date: "2024-01-01T00:00:00Z"
+    }
+  };
+  $done({ body: JSON.stringify(body) });
 }
