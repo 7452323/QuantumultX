0bbb487692adeefc9effb40934b7ce82cf92757b
7452323 <7452323@users.noreply.github.com>
2026-06-23 09:01:56 -0400

    fix: check  before parsing body

--- script/Retouch.js
+++ script/Retouch.js
@@ -10,34 +10,36 @@
 [mitm]
 hostname = api.revenuecat.com
 */
-var body = JSON.parse($response.body);
-if ($response && body && body.subscriber) {
-  var sub = body.subscriber;
-  sub.subscriptions = {
-    tr5_yearlysubsc_20dlrs_1: {
-      expires_date: "2099-12-31T23:59:59Z",
-      original_purchase_date: "2023-01-01T00:00:00Z",
-      purchase_date: "2024-01-01T00:00:00Z",
-      ownership_type: "PURCHASED",
-      store: "app_store",
-      is_sandbox: false,
-      unsubscribe_detected_at: null,
-      billing_issues_detected_at: null
-    }
-  };
-  sub.entitlements = {
-    premium: {
-      expires_date: "2099-12-31T23:59:59Z",
-      product_identifier: "tr5_yearlysubsc_20dlrs_1",
-      purchase_date: "2024-01-01T00:00:00Z"
-    }
-  };
-  body.subscriber = sub;
-  $done({ body: JSON.stringify(body) });
-} else {
+if (typeof $response == "undefined") {
   delete $request.headers["if-none-match"];
   delete $request.headers["If-None-Match"];
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
   $done({ headers: $request.headers });
+} else {
+  var body = JSON.parse($response.body);
+  if (body && body.subscriber) {
+    body.subscriber.subscriptions = {
+      tr5_yearlysubsc_20dlrs_1: {
+        expires_date: "2099-12-31T23:59:59Z",
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        purchase_date: "2024-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        store: "app_store",
+        is_sandbox: false,
+        unsubscribe_detected_at: null,
+        billing_issues_detected_at: null
+      }
+    };
+    body.subscriber.entitlements = {
+      premium: {
+        expires_date: "2099-12-31T23:59:59Z",
+        product_identifier: "tr5_yearlysubsc_20dlrs_1",
+        purchase_date: "2024-01-01T00:00:00Z"
+      }
+    };
+    $done({ body: JSON.stringify(body) });
+  } else {
+    $done({});
+  }
 }
